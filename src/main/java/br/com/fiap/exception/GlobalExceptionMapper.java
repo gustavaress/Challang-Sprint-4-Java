88f89e9f3ee8fe252e2882dbe47e035669cc0e21
gustavaress <gustavares2005@gmail.com>
2025-11-07 17:18:57 -0300

    Adicionando correção no feedback do erro 404, que antes retornava como um erro interno. Tambem foi adicionado um HomeResource para termos um retorno ao acessarmos o endpoint raiz da api

--- src/main/java/br/com/fiap/exception/GlobalExceptionMapper.java
+++ src/main/java/br/com/fiap/exception/GlobalExceptionMapper.java
@@ -1,5 +1,6 @@
 package br.com.fiap.exception;
 
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 import jakarta.ws.rs.core.Response;
@@ -9,10 +10,16 @@ public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
 
     @Override
     public Response toResponse(Exception e) {
-        e.printStackTrace(); // opcional: remove em produção
+        e.printStackTrace();
+
+        if (e instanceof NotFoundException) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErroResposta("404. Rota não encontrada."))
+                    .build();
+        }
 
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(new ErroResposta("Erro interno no servidor."))
+                .entity(new ErroResposta("500. Erro interno no servidor."))
                 .build();
     }
 }
